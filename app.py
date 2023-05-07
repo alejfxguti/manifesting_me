79694a329d8c2ee5c9acf82287c941537fbe6f2c
alejfxguti <alej.fx.guti@gmail.com>
2023-05-06 17:41:42 -0700

    Fixing the flask app with test

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///manifestation.db'
 db = SQLAlchemy(app)
 
+if __name__ == '__main__':
+    app.run(debug=True)
